73952a17131f44088be7cbcbcf3a31b47f9bd408
prajsh <prajsh.axelor@gmail.com>
2019-09-16 11:06:51 +0530

    some changes according to question no 1

--- modules/axelor-event/src/main/java/com/axelor/apps/event/web/EventRegistrationController.java
+++ modules/axelor-event/src/main/java/com/axelor/apps/event/web/EventRegistrationController.java
@@ -1,5 +1,7 @@
 package com.axelor.apps.event.web;
 
+
+import com.axelor.apps.event.db.Event;
 import com.axelor.apps.event.db.EventRegistration;
 import com.axelor.apps.event.service.EventRegistrationService;
 import com.axelor.rpc.ActionRequest;
@@ -11,9 +13,25 @@ public class EventRegistrationController {
 	EventRegistrationService eventRegistrationService;
 	
 	public void calculateAmount(ActionRequest request, ActionResponse response) {
+		
 		 EventRegistration eventReg= request.getContext().asType(EventRegistration.class);
-		 eventReg.setAmount(eventRegistrationService.calculateAmount(eventReg));
-		 response.setValues(eventReg);
+		 
+		 if(eventReg.getEvent() != null) {
+			 eventReg.setAmount(eventRegistrationService.calculateAmount(eventReg));
+			 response.setValues(eventReg);
+		 }
+	}
+	
+	public void setParentEvent(ActionRequest request, ActionResponse response) {
+		
+		 EventRegistration eventReg= request.getContext().asType(EventRegistration.class);
+		 
+		 if(eventReg.getEvent() == null) {
+			 Event event = request.getContext().getParent().asType(Event.class);
+			 eventReg.setEvent(event);
+			 System.out.println("Event = "+eventReg.getEvent());
+			 response.setValues(eventReg);
+		 }
 	}
 	
 }
